696931e43c39d66aeca4b3686cb469562f9702b6
User <liannapyman@gmail.com>
2022-07-27 12:31:15 +0930

    Adds async

--- src/components/Search-Results/Results.jsx
+++ src/components/Search-Results/Results.jsx
@@ -13,11 +13,18 @@ class Results extends React.Component {
   }
 
   handleFetch = async (searchTerm) => {
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=40`;
-
-    const response = await fetch(url);
-    const json = await response.json();
-    return json.items;
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=40`
+    );
+    const data = await response.json();
+    const { items } = await data;
+    const books = items.map(async ({ volumeInfo }) => {
+      const { title, authors, description, imageLinks } = await volumeInfo;
+      const thumbnail = imageLinks === undefined ? "" : imageLinks.thumbnail;
+      return { title, authors, description, thumbnail };
+    });
+    const updated = Promise.all(books);
+    this.setState({ books: updated });
   };
 
   handleSearch = (e) => {
@@ -26,28 +33,28 @@ class Results extends React.Component {
     this.setState({ searchTerm: e.target.value });
   };
 
-  cleanData = (data) => {
-    const cleanedData = data.body.items.map((book) => {
-      if (book.volumeInfo.hasOwnProperty("publishedDate") === false) {
-        book.volumeInfo["publishDate"] = "N/A";
-      } else if (book.volumeInfo.hasOwnProperty("imageLinks") === false) {
-        book.volumeInfo["imageLinks"] = {
-          thumbnail:
-            "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg?ver=6",
-        };
-      }
+  // cleanData = (data) => {
+  //   const cleanedData = data.body.items.map((book) => {
+  //     if (book.volumeInfo.hasOwnProperty("publishedDate") === false) {
+  //       book.volumeInfo["publishDate"] = "N/A";
+  //     } else if (book.volumeInfo.hasOwnProperty("imageLinks") === false) {
+  //       book.volumeInfo["imageLinks"] = {
+  //         thumbnail:
+  //           "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg?ver=6",
+  //       };
+  //     }
 
-      return book;
-    });
-    return cleanedData;
-  };
+  //     return book;
+  //   });
+  //   return cleanedData;
+  // };
 
   render() {
     return (
       <div className={styles.Results}>
         <SearchBar
           handleSearch={this.handleSearch}
-          fetchBooks={this.fetchBooks}
+          handleFetch={this.handleFetch}
           className={styles.Search}
         />
         <BookList books={this.state.books} />
