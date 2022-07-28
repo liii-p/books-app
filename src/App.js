c94adcad81c1988c91a2c439b2b44a924987a1da
User <liannapyman@gmail.com>
2022-07-28 14:29:19 +0930

    Changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import styles from "./App.module.scss";
 import React from "react";
 import Header from "./Header";
 import Results from "./components/Search-Results/Results";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <Results />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar onSubmit={handleSubmit} />
+              </>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Search-Results/Results.jsx
+++ src/components/Search-Results/Results.jsx
@@ -2,65 +2,14 @@ import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import BookList from "./BookList";
 import styles from "../Search-Results/Results.module.scss";
+import { useState } from "react";
+import { useEffect } from "react";
 
-class Results extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-      searchTerm: "",
-    };
-  }
+const Results = () => {
+  const [search, setSearch] = useState("");
+  const [displayBooks, setDisplayBooks] = useState([]);
 
-  handleFetch = async (searchTerm) => {
-    const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=40`
-    );
-    const data = await response.json();
-    const { items } = await data;
-    const books = items.map(async ({ volumeInfo }) => {
-      const { title, authors, description, imageLinks } = await volumeInfo;
-      const thumbnail = imageLinks === undefined ? "" : imageLinks.thumbnail;
-      return { title, authors, description, thumbnail };
-    });
-    const updated = Promise.all(books);
-    this.setState({ books: updated });
-  };
-
-  handleSearch = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-    this.setState({ searchTerm: e.target.value });
-  };
-
-  // cleanData = (data) => {
-  //   const cleanedData = data.body.items.map((book) => {
-  //     if (book.volumeInfo.hasOwnProperty("publishedDate") === false) {
-  //       book.volumeInfo["publishDate"] = "N/A";
-  //     } else if (book.volumeInfo.hasOwnProperty("imageLinks") === false) {
-  //       book.volumeInfo["imageLinks"] = {
-  //         thumbnail:
-  //           "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg?ver=6",
-  //       };
-  //     }
-
-  //     return book;
-  //   });
-  //   return cleanedData;
-  // };
-
-  render() {
-    return (
-      <div className={styles.Results}>
-        <SearchBar
-          handleSearch={this.handleSearch}
-          handleFetch={this.handleFetch}
-          className={styles.Search}
-        />
-        <BookList books={this.state.books} />
-      </div>
-    );
-  }
-}
+  return <></>;
+};
 
 export default Results;
